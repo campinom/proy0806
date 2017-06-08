903e72761c9a277febe1f02c7866e4c87ca2bc1f
cetecom <cetecom@LC08L1222>
2017-06-08 13:33:24 -0400

    Morido en Dolor

--- index.php
+++ index.php
@@ -7,19 +7,18 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <script src="jquery-3.2.1.min.js" type="text/javascript"></script>
+        <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
         <title></title>
     </head>
     <body>
-        <form id="frmusuario">
+        <form id="frmusuario" action="controlador/ValidaUsuario.php" method="post">
             <input type="text" name="nomusuario" id="nomusuario">
             <input type="password" name="clave" id="clave">
             <input type="button" onclick="" value="Enviar" id="enviar">
+            <div id="mensaje"></div>
 
         </form>
-        <?php
-        // put your code here
-        ?>
+        
     </body>
     <script>
         $(document).ready(function(){
@@ -27,9 +26,19 @@ and open the template in the editor.
                 /*$("form").hide();
                 alert("Te la echaste HERMANITOOOOOOOOOOOOOOOOOOO");*/
         
-                if($("#nomusuario").val()!= "" && $(#clave).val()!=""){
-                    $("#frmusuario").submit();
-                
+              
+                if($("#nomusuario").val()!= "" && $("#clave").val()!=""){
+                   ///* $("#frmusuario").submit();
+                       $.ajax({url:"controlador/ValidaUsuario.php"
+                            ,type:'post'
+                            ,data:{'nomusuario':$("nomusuario").val(),
+                                'clave':$("#clave").val()
+                            }
+                            
+                            ,sucess:function(resultado){
+                            $("#mensaje").html(resultado);
+                          
+        }});}
                 else
                     alert("Escrie el nombre de usuario y clave oe");
                 
