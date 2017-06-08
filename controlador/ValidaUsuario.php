8496603298f1b69ca2ed84fc700c041e3f01c81f
cetecom <cetecom@LC08L1222>
2017-06-08 13:36:14 -0400

    Mi proyecto no se sube completo :(

--- controlador/ValidaUsuario.php
+++ controlador/ValidaUsuario.php
@@ -2,4 +2,13 @@
 
 include '../librerias.php';
 
+$oUsu=new Usuario($_REQUEST["nomusuario"],$_REQUEST["clave"]);
+
+if($oUsu->VerificaLocal())
+    echo "TODO OKEI";
+    
+    else
+        echo "Todo mal :( uwu ";
+        
+
 
---
+++ librerias.php
@@ -0,0 +1,13 @@
+<?php
+
+
+include 'constantes.php';
+include PATH.'/Proy0806/lib/Conexion.php';
+include PATH.'/Proy0806/lib/Usuario.php';
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
